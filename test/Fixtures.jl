e03c8ce68e04c98c1485463a9275495f7a9e80c4
David Hanak <dhanak@gmail.com>
2023-04-25 15:13:08 +0200

    test: Drop @pattern macros altogether

--- test/Fixtures.jl
+++ test/Fixtures.jl
@@ -22,46 +22,9 @@ using FeatureScreening.Types: FeatureSet
 ### Implementation
 ###=============================================================================
 
-macro with_pattern(f)
-    @assert f.head == :function
-    f_name::Symbol = f.args[1]
-    return quote
-        $f
-        function $f_name(tokens::Symbol...)
-            try
-                return $f_name(Val.(tokens)...)
-            catch e
-                if e isa MethodError
-                    @error "Missing pattern $(e.f)($(e.args...))"
-                end
-                rethrow(e)
-            end
-        end
-    end |> esc
-end
+fixture(arg::Symbol) = fixture(Val(arg))
 
-macro pattern(f)
-    @assert f.head == :function
-    (signature, body...) = f.args
-    @assert(f.args[1].head == :call,
-            "Simple function signature allowed(, no return type assertion)!")
-    (function_name::Symbol, tokens...) = f.args[1].args
-    arguments::Vector = map(tokens) do token
-        return :(::Val{$token})
-    end
-
-    return quote
-        function $function_name($(arguments...))
-            return $(body...)
-        end
-    end |> esc
-end
-
-##------------------------------------------------------------------------------
-
-@with_pattern function fixture end
-
-@pattern function fixture(:config, :screen)
+function fixture(::Val{:screen_config})
     return (n_subfeatures       = -1,
             n_trees             = 20,
             partial_sampling    = 0.9,
@@ -71,11 +34,11 @@ end
             min_purity_increase = 0.0)
 end
 
-@pattern function fixture(:feature_set)
+function fixture(::Val{:feature_set})
     return FeatureSet(fixture(:y), fixture(:names), fixture(:X))
 end
 
-@pattern function fixture(:y)
+function fixture(::Val{:y})
     return [:a, :a, :a, :a, :a,
             :b, :b, :b, :b, :b,
             :c, :c, :c, :c, :c,
@@ -83,11 +46,11 @@ end
             :e, :e, :e, :e, :e]
 end
 
-@pattern function fixture(:names)
-    return ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+function fixture(::Val{:names})
+    return ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"]
 end
 
-@pattern function fixture(:X)
+function fixture(::Val{:X})
     X::Matrix{Float64} =
         [0.0 -0.2 -0.4 -0.6 -0.8 -1.0 -1.2 -1.4 -1.6 -1.8 -2.0;
          0.0 -0.2 -0.4 -0.6 -0.8 -1.0 -1.2 -1.4 -1.6 -1.8 -2.0;
--- test/runtests.jl
+++ test/runtests.jl
@@ -25,7 +25,6 @@ Test.get_alignment(::Test.DefaultTestSet, ::Int) = 30
 # fixed random seed
 seed!(1)
 
-include("test.fixtures.jl")
 include("test.basics.jl")
 include("test.feature-set.jl")
 include("test.importance.jl")
