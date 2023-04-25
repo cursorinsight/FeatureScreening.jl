6e358ce94746189b39b148a51481fe5c24841492
David Hanak <dhanak@gmail.com>
2023-04-25 15:13:44 +0200

    test: Slightly increase test coverage

--- test/test.basics.jl
+++ test/test.basics.jl
@@ -18,42 +18,47 @@ using FeatureScreening: screen
 @testset "Basics" begin
 
     @testset "API #1" begin
-        LABEL_COUNT::Int    = 5
-        SAMPLE_COUNT::Int   = 5
-        FEATURE_COUNT::Int  = 11
-        STEP_SIZE::Int      = 3
-        REDUCED_SIZE::Int   = 3
+        LABEL_COUNT::Int   = 5
+        SAMPLE_COUNT::Int  = 5
+        FEATURE_COUNT::Int = 11
+        STEP_SIZE::Int     = 3
+        REDUCED_SIZE::Int  = 3
 
-        selected = screen(fixture(:feature_set);
-                          reduced_size  = REDUCED_SIZE,
-                          step_size     = STEP_SIZE,
-                          config        = fixture(:config, :screen),
-                          show_progress = false)
+        @testset for shuffle in [false, true]
+            selected = screen(fixture(:feature_set);
+                              reduced_size  = REDUCED_SIZE,
+                              step_size     = STEP_SIZE,
+                              config        = fixture(:screen_config),
+                              show_progress = false,
+                              shuffle)
 
-        @test selected isa FeatureSet{Symbol, String, Float64}
-        @test LABEL_COUNT * SAMPLE_COUNT == size(selected, 1)
-        @test REDUCED_SIZE == size(selected, 2)
-        @test_skip names(selected) ⊆ ["8", "9", "10", "11"]
+            @test selected isa FeatureSet{Symbol, String, Float64}
+            @test LABEL_COUNT * SAMPLE_COUNT == size(selected, 1)
+            @test REDUCED_SIZE == size(selected, 2)
+        end
     end
 
     @testset "API #2" begin
-        LABEL_COUNT::Int    = 5
-        SAMPLE_COUNT::Int   = 5
-        FEATURE_COUNT::Int  = 11
-        STEP_SIZE::Int      = 3
-        REDUCED_SIZE::Int   = 3
+        LABEL_COUNT::Int   = 5
+        SAMPLE_COUNT::Int  = 5
+        FEATURE_COUNT::Int = 11
+        STEP_SIZE::Int     = 3
+        REDUCED_SIZE::Int  = 3
 
-        selected = screen(fixture(:X),
-                          fixture(:y);
-                          reduced_size  = REDUCED_SIZE,
-                          step_size     = STEP_SIZE,
-                          config        = fixture(:config, :screen),
-                          show_progress = false)
+        @testset for shuffle in [false, true]
+            selected = screen(fixture(:X),
+                              fixture(:y);
+                              reduced_size  = REDUCED_SIZE,
+                              step_size     = STEP_SIZE,
+                              config        = fixture(:screen_config),
+                              show_progress = false,
+                              rng           = 1234,
+                              shuffle)
 
-        @test selected isa FeatureSet{Symbol, Int, Float64}
-        @test LABEL_COUNT * SAMPLE_COUNT == size(selected, 1)
-        @test REDUCED_SIZE == size(selected, 2)
-        @test_skip names(selected) ⊆ 8:11
+            @test selected isa FeatureSet{Symbol, Int, Float64}
+            @test LABEL_COUNT * SAMPLE_COUNT == size(selected, 1)
+            @test REDUCED_SIZE == size(selected, 2)
+        end
     end
 
 end
